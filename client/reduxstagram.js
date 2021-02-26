958155ca2b55968bd36b25885a91308719ff0393
Zan <zanasmakarov@gmail.com>
2021-02-26 14:01:10 +0000

    added router component to navigate between grid and single on the rendered main route

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -4,5 +4,23 @@ import { render } from 'react-dom';
 import css from './styles/style.styl';
 //components
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
 
-render(<Main/>, document.getElementById('root'));
+//react router deps
+import { Router, Route, IndexRoute, browserHistory } from "react-router";
+
+const router = (
+    //will alow push state and change urls without reloading the page
+    <Router history={browserHistory}>
+        <Route path="/" component={Main}>
+            //if / then:
+            <IndexRoute component={PhotoGrid}></IndexRoute>
+            //other routes
+            <Route path="/view/:postId" component={Single}></Route>
+        </Route>
+    </Router>
+);
+
+
+render(router, document.getElementById('root'));
